8a4d47a906d6aa1a21b624e20b46d17aed3275e4
xiaosy <siyong.xiao@ishansong.com>
2020-02-27 18:36:51 +0800

    数据同步服务系统化

--- algorithm/slice_rotate.go
+++ algorithm/slice_rotate.go
@@ -4,9 +4,9 @@ import "fmt"
 
 /**
 二维数组旋转90度  可能是长方形的矩阵
- */
-func sliceRotate(a [][]int)[][]int{
+*/
+func sliceRotate(a [][]int) [][]int {
 	l := len(a)
-	fmt.Println(l)h
+	fmt.Println(l)
 	return a
 }
